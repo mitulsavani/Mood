7811b39d719cfd2abfef286f408170a395b37913
Mitul Savani <mitulsavani@FLM-MBP-AGREEYA12-2.local>
2022-01-27 22:17:38 -0800

    added component, props, and usecallback

---
+++ src/components/MoodPicker.tsx
@@ -0,0 +1,112 @@
+import React from 'react';
+import { View, Text, StyleSheet, Pressable, Button } from 'react-native';
+import { MoodOptionType } from '../types.ts';
+import {theme} from '../theme.ts';
+
+const moodOptions: MoodOptionType[] = [
+  { emoji: '🧑‍💻', description: 'Prepping' },
+  { emoji: '🤔', description: 'Missing Angel' },
+  { emoji: '😊', description: 'Happy' },
+  { emoji: '🥳', description: 'Offer' },
+  { emoji: '😤', description: 'Exhausted' },
+];
+
+type MoodPickerProps = {
+  onSelect: (mood: MoodOptionType) => void;
+}
+
+export const MoodPicker: React.FC<MoodPickerProps> = ({ onSelect }) => {
+  const [selectedMood, setSelectedMood] = React.useState<MoodOptionType>();
+
+  const handleSelect = React.useCallback(() => {
+    if(selectedMood) {
+      onSelect(selectedMood);
+      setSelectedMood(undefined);
+    }
+  }, [onSelect, selectedMood]);
+
+  return (
+    <View style={styles.container}>
+    <Text style={styles.heading}>How are you right now?</Text>
+    <View style={styles.moodList}>
+      {moodOptions.map(option => (
+        <View>
+          <Pressable
+            onPress={() => setSelectedMood(option)}
+            key={option.emoji}
+            style={[
+              styles.moodItem,
+              option.emoji === selectedMood?.emoji
+                ? styles.selectedMoodItem
+                : undefined,
+            ]}>
+            <Text style={styles.moodText}>{option.emoji}</Text>
+          </Pressable>
+          <Text style={styles.descriptionText}>
+            {selectedMood?.emoji === option.emoji ? option.description : ' '}
+          </Text>
+        </View>
+      ))}
+    </View>
+    <Pressable style={styles.button} onPress={handleSelect}>
+      <Text style={styles.buttonText}>Choose</Text>
+    </Pressable>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    margin: 10,
+    padding: 20,
+    borderWidth: 2,
+    borderRadius: 10,
+    borderColor: theme.colorPurple,
+  },
+  heading: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    textAlign: 'center',
+    letterSpacing: 1,
+    marginBottom: 20,
+  },
+  moodText: {
+    fontSize: 24,
+  },
+  moodItem: {
+    width: 60,
+    height: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 30,
+    marginBottom: 5,
+  },
+  selectedMoodItem: {
+    borderWidth: 2,
+    backgroundColor: theme.colorPurple,
+    borderColor: theme.colorWhite,
+  },
+  moodList: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  descriptionText: {
+    color: '#454C73',
+    fontWeight: 'bold',
+    fontSize: 10,
+    textAlign: 'center',
+  },
+  button: {
+    backgroundColor: theme.colorPurple,
+    width: 150,
+    borderRadius: 20,
+    marginTop: 20,
+    alignSelf: 'center',
+    padding: 10,
+  }, 
+  buttonText: {
+    color: theme.colorWhite,
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+});
--- src/screens/Home.screen.tsx
+++ src/screens/Home.screen.tsx
@@ -1,16 +1,30 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
+import { MoodPicker } from "../components/MoodPicker";
+import { MoodOptionType, MoodOptionWithTimestamp } from '../types.ts';
 
 export const Home: React.FC = () => {
+
+  const [moodList, setMoodList] = React.useState<MoodOptionWithTimestamp[]>([]);
+  const handleSelectMood = React.useCallback((mood: MoodOptionType) => {
+    setMoodList((current) => [...current, {mood, timestamp: Date.now()}]);
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Home Screen</Text>
+      <MoodPicker onSelect={handleSelectMood}/>
+      {moodList.map((item) => (
+        <Text key={item.timestamp}>
+          {item.mood.emoji} {new Date(item.timestamp).toString()}
+        </Text>
+      ))}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    justifyContent: 'center',
     flex: 1,
   },
 });
