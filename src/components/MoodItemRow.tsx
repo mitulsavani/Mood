4c21dce6bd98b708abba6bb702c9169f964b6b27
Mitul Savani <mitulsavani@FLM-MBP-AGREEYA12-2.local>
2022-03-09 12:00:10 -0800

    added delete history using gesture

--- src/components/MoodItemRow.tsx
+++ src/components/MoodItemRow.tsx
@@ -10,32 +10,86 @@ import { useAppContext } from '../App.provider';
 import format from 'date-fns/format';
 import { MoodOptionWithTimestamp } from '../types.ts';
 import { theme } from '../theme.ts';
+import { PanGestureHandler, PanGestureHandlerGestureEvent } from 'react-native-gesture-handler';
+import Reanimated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+  useAnimatedGestureHandler,
+  runOnJS,
+} from 'react-native-reanimated';
 
 type MoodItemRowProps = {
   item: MoodOptionWithTimestamp;
 };
 
+const maxPan = 80;
+
 export const MoodItemRow: React.FC<MoodItemRowProps> = ({ item }) => {
   const appContext = useAppContext();
+  const offset = useSharedValue(0);
 
   const handlePress = React.useCallback(() => {
     LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
     appContext.handleDeleteMood(item);
   }, [appContext, item]);
 
+  const animatedStyle = useAnimatedStyle(() => ({
+    transform: [{ translateX: offset.value }],
+  }));
+
+  const removeWithDelay = React.useCallback(() => {
+    setTimeout(() => {
+      LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
+      appContext.handleDeleteMood(item);
+    }, 250);
+  }, [appContext, item]);
+
+  const onGestureEvent = useAnimatedGestureHandler<
+    PanGestureHandlerGestureEvent,
+    { shouldRemove: boolean }
+  >(
+    {
+      onActive: (event, ctx) => {
+        const xVal = Math.floor(event.translationX);
+        offset.value = xVal;
+
+        if (Math.abs(xVal) <= maxPan) {
+          ctx.shouldRemove = false;
+        } else {
+          ctx.shouldRemove = true;
+        }
+      },
+      onEnd: (_, ctx) => {
+        if (ctx.shouldRemove) {
+          offset.value = withTiming(Math.sign(offset.value) * 2000);
+          runOnJS(removeWithDelay)();
+        } else {
+          offset.value = withTiming(0);
+        }
+      },
+    },
+    [],
+  );
+
   return (
-    <View style={styles.moodItem}>
-      <View style={styles.iconAndDescription}>
-        <Text style={styles.moodValue}>{item.mood.emoji}</Text>
-        <Text style={styles.moodDescription}> {item.mood.description}</Text>
-      </View>
-      <Text style={styles.moodDate}>
-        {format(new Date(item.timestamp), "dd MMM, yyy 'at' h:mmaaa")}
-      </Text>
-      <Pressable hitSlop={16} onPress={handlePress}>
-        <Text style={styles.deleteText}>Delete</Text>
-      </Pressable>
-    </View>
+    <PanGestureHandler
+      minDeltaX={1}
+      minDeltaY={100}
+      onGestureEvent={onGestureEvent}>
+      <Reanimated.View style={[styles.moodItem, animatedStyle]}>
+        <View style={styles.iconAndDescription}>
+          <Text style={styles.moodValue}>{item.mood.emoji}</Text>
+          <Text style={styles.moodDescription}> {item.mood.description}</Text>
+        </View>
+        <Text style={styles.moodDate}>
+          {format(new Date(item.timestamp), "dd MMM, yyy 'at' h:mmaaa")}
+        </Text>
+        <Pressable hitSlop={16} onPress={handlePress}>
+          <Text style={styles.deleteText}>Delete</Text>
+        </Pressable>
+      </Reanimated.View>
+    </PanGestureHandler>
   );
 };
 
